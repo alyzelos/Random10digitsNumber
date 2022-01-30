17d4e07051f2a1823e53c0906dbd233b07d9b93c
alyzelos <pojarcosminalin@gmail.com>
2022-01-30 12:26:40 +0200

    files commit

---
+++ RandomPhoneNumber/MethodOfGeneratingNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using.System.Random;
+using ImTools;
+
+namespace RandomPhoneNumber
+{
+
+    class MethodOfGeneratingNumber
+    {
+        private int variabila = new Random();
+
+        int[] randomTelephoneNumber = new int[10];
+       
+        for (int i = 0; I<10; i++)
+			{
+             
+
+            }
+}
+}
