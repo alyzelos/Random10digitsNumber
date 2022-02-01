0b8899728ea5345cfd9f30a91c7702e122618548
alyzelos <pojarcosminalin@gmail.com>
2022-02-01 12:52:59 +0200

    app modif Com

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string numarAleatorPtTelefon { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RandomNumberGenerator numarAleator = new RandomNumberGenerator();
+            numarAleatorPtTelefon = numarAleator.RandomTelephoneGenerator();
+            textBox1.Text = numarAleatorPtTelefon;
+            MessageBox.Show("New 10 digits random number generated");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ WindowsFormsApp1/RandomNumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    public class RandomNumberGenerator
+    {
+        public string myStringValue { get; set; }
+        public int[] randomNumber = new int[10];
+        public string RandomTelephoneGenerator()
+        {
+            
+            
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                randomNumber[i] = random.Next(0, 10);
+
+            }
+           // cacavaloare = string.Join(" ", Convert.ToString(randomNumber);
+
+            foreach (var valoareIndividuala in randomNumber)
+            {
+               myStringValue =  myStringValue + String.Join(" ", Convert.ToString(valoareIndividuala));
+            }
+            return myStringValue;
+        }
+    }
+}
